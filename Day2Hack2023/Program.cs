678516d41eae2119167ec52c6027c1cdb5832f02
TheFirest-Artemis <artem.rvachev2@gmail.com>
2023-08-12 12:11:34 +0300

    create files

---
+++ Day2Hack2023/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Day2Hack2023
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
