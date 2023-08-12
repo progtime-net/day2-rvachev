420f2c0be47f2c55c3fd56dcf60c864233ad726d
TheFirest-Artemis <artem.rvachev2@gmail.com>
2023-08-12 12:21:45 +0300

    hi func

--- Day2Hack2023/Program.cs
+++ Day2Hack2023/Program.cs
@@ -4,7 +4,9 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Как тебя зовут?");
+            string name = Console.ReadLine();
+            Console.WriteLine(name);
         }
     }
 }
